23237e42184a763ce9cf9b968374f3cd334a2059
Ezio <123456@qq.com>
2020-09-23 09:38:35 +0800

    修改user模块的models

--- User/models.py
+++ User/models.py
@@ -3,11 +3,25 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    nickname = models.CharField(max_length=16)
-    phonenum = models.CharField(max_length=11)
-    birthday = models.DateField(default='2000-01-01')
-    gender = models.CharField(max_length=8, choices=(('male', '男'), ('female', '女')), default='male')
-    location = models.CharField(default='北京', max_length=16)
+    GENDERS = (
+        ('male', '男性'),
+        ('female', '女性'),
+    )
+    LOCATIONS = (
+        ('北京', '北京'),
+        ('上海', '上海'),
+        ('深圳', '深圳'),
+        ('成都', '成都'),
+        ('西安', '西安'),
+        ('武汉', '武汉'),
+        ('沈阳', '沈阳'),
+    )
+    phonenum = models.CharField(max_length=16, verbose_name='手机号', unique=True)
+    nickname = models.CharField(max_length=32, verbose_name='昵称', db_index=True)
+    gender = models.CharField(max_length=8, choices=GENDERS, default='male', verbose_name='性别')
+    birthday = models.DateField(default='2002-01-01', verbose_name='出生日')
+    avatar = models.CharField(default='', max_length=256, verbose_name='个人形象')
+    location = models.CharField(default='北京', max_length=16, choices=LOCATIONS, verbose_name='常居地')
 
     class Meta:
         db_table = 'user'
